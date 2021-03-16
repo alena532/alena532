a8b50f1132831ea5ed398172e42e37dbc53786ec
alena532 <79169496+alena532@users.noreply.github.com>
2021-03-16 20:38:07 +0300

    Add files via upload

---
+++ лаба3/main.h
@@ -0,0 +1,4 @@
+void setVertically(char field[10][10],int x,int y,int deck);
+void setHorizontal(char field[10][10],int x,int y,int deck);
+int vertically(char field[10][10],int x,int y,int deck);
+int horizontal(char field[10][10],int x,int y,int deck);
